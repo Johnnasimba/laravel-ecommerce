b2ddc0b72f7f4a9716ec0ce53f079f73da216d95
John Nasimba <nasimba4john@gmail.com>
2024-01-20 15:29:35 +0200

    update profile

---
+++ resources/views/admin/profile/index.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.layouts.master')
+
+@section('content')
+	<section class="section">
+		<div class="section-header">
+			<h1>Profile</h1>
+			<div class="section-header-breadcrumb">
+				<div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+				<div class="breadcrumb-item">Profile</div>
+			</div>
+		</div>
+		<div class="section-body">
+			<div class="row mt-sm-4">
+				<div class="col-12 col-md-12 col-lg-7">
+					<div class="card">
+						<form method="post" action="{{route('admin.profile.update')}}"
+									class="needs-validation"
+									novalidate=""
+						>
+							@csrf
+							<div class="card-header">
+								<h4>Update Profile</h4>
+							</div>
+							<div class="card-body">
+								<div class="row">
+									<div class="form-group col-md-6 col-12">
+										<label>Name</label>
+										<input type="text" name="name" class="form-control"
+													 value="{{Auth::user()->name}}" required="">
+									</div>
+									<div class="form-group col-md-6 col-12">
+										<label>Email</label>
+										<input type="email" name="email" class="form-control"
+													 value="{{Auth::user()->email}}" required="">
+									</div>
+								</div>
+							</div>
+							<div class="card-footer text-right">
+								<button class="btn btn-primary">Save Changes</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+@endsection
